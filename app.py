fcf2092e4d4d5528202fd12ab1006d7751bdacab
uwuok <natsumi7129453@gmail.com>
2024-11-10 03:39:53 +0800

    update

---
+++ app.py
@@ -0,0 +1,37 @@
+
+import lib
+
+def main():
+    lib.create_table()
+    while True:
+        print()
+        print(f"{'-' * 5} 電影管理系統 {'-' * 5}")
+        print('1. 匯入電影資料檔')
+        print('2. 查詢電影')
+        print('3. 新增電影')
+        print('4. 修改電影')
+        print('5. 刪除電影')
+        print('6. 匯出電影')
+        print('7. 離開系統')
+        print(f"{'-' * 24}")
+        select = int(input('請選擇操作選項 (1-7): '))
+        if select == 1:
+            lib.import_movies()
+        elif select == 2:
+            lib.search_movie()
+        elif select == 3:
+            lib.add_movie()
+        elif select == 4:
+            lib.modify_movies()
+        elif select == 5:
+            lib.delete_movies()
+        elif select == 6:
+            lib.export_movies()
+        else:
+            print('系統已退出。')
+            exit()
+            
+
+if __name__ == "__main__":
+    main()
+    
---
+++ lib.py
@@ -0,0 +1,244 @@
+import sqlite3
+import json
+
+DB_PATH = 'movies.db'
+JSON_IN_PATH = 'movies.json'
+JSON_OUT_PATH = 'exported.json'
+
+def print_movie_row(row):
+    print(
+        f"{row['title']:{chr(12288)}<10}"
+        f"{row['director']:{chr(12288)}<12}"
+        f"{row['genre']:{chr(12288)}<5}"
+        f"{row['year']:{chr(12288)}<8}"
+        f"{row['rating']:{chr(12288)}<10}"
+    )
+
+def list_rpt():
+    header = (
+        f"{'電影名稱'}{chr(12288) * 6}"
+        f"{'導演'}{chr(12288) * 10}"
+        f"{'類型'}{chr(12288) * 3}"
+        f"{'上映年份'}{chr(12288) * 2}"
+        f"{'評分'}{chr(12288) * 2}"
+    )
+    separator = '─' * 80
+    print()
+    print(header)
+    print(separator)
+
+def create_table():
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS movies (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                director TEXT NOT NULL,
+                genre TEXT NOT NULL,
+                year INTEGER NOT NULL,
+                rating REAL CHECK(rating >= 1.0 AND rating <= 10.0)
+                )
+            ''')
+        except sqlite3.DatabaseError as e:
+            print(f'資料庫操作發生錯誤： {e}')
+        except Exception as e:
+            print(f'發生其它錯誤 {e}')
+
+def import_movies():
+    try:
+        with open(JSON_IN_PATH, 'r', encoding='UTF-8') as f:
+            movies = json.load(f)
+    except FileNotFoundError:
+        print(f"檔案 {JSON_IN_PATH} 未找到！")
+        return
+    except json.JSONDecodeError:
+        print(f"檔案 {JSON_IN_PATH} 內容格式錯誤！")
+        return
+    except Exception as e:
+        print(f'發生其它錯誤 {e}')
+        return
+
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        for movie in movies:
+            try:
+                cursor.execute("""
+                    INSERT INTO movies(title, director, genre, year, rating)
+                    VALUES (?, ?, ?, ?, ?)
+                """, (movie['title'], movie['director'], movie['genre'], movie['year'], movie['rating']))
+            except sqlite3.IntegrityError:
+                print(f"電影 {movie['title']} 已存在，跳過匯入")
+            except sqlite3.DatabaseError as e:
+                print(f'資料庫操作發生錯誤： {e}')
+            except Exception as e:
+                print(f'發生其它錯誤 {e}')
+        conn.commit()
+    print('電影已匯入')
+
+def search_movie():
+    y_n = input('查詢全部電影嗎？(y/n): ')
+    
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        try:
+            if y_n.lower() == 'y':
+                cursor.execute('SELECT * FROM movies')
+            else:
+                title = input('請輸入電影名稱： ')
+                cursor.execute('SELECT * FROM movies WHERE title LIKE ?', (f'%{title}%',))
+            results = cursor.fetchall()
+
+            if results:
+                list_rpt()
+                for row in results:
+                    print_movie_row(row)
+            else:
+                print("找不到符合條件的電影")
+        except sqlite3.DatabaseError as e:
+            print(f'資料庫操作發生錯誤： {e}')
+        except Exception as e:
+            print(f'發生其它錯誤 {e}')
+
+def add_movie():
+    try:
+        title = input('電影名稱： ')
+        director = input('導演： ')
+        genre = input('類型： ')
+        year = int(input('上映年份： '))
+        rating = float(input('評分 (1.0 - 10.0)： '))
+
+        with connect_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute("""
+                INSERT INTO movies (title, director, genre, year, rating)
+                VALUES (?, ?, ?, ?, ?)
+            """, (title, director, genre, year, rating))
+        print('電影已新增')
+    except sqlite3.DatabaseError as e:
+        print(f'資料庫操作發生錯誤： {e}')
+    except ValueError:
+        print("請確認輸入的年份和評分是有效的數值！")
+    except Exception as e:
+        print(f'發生其它錯誤 {e}')
+
+def modify_movies():
+    title = input('請輸入要修改的電影名稱： ')
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM movies WHERE title LIKE ?', (f'%{title}%',))
+            result = cursor.fetchall()
+        except sqlite3.DatabaseError as e:
+            print(f'資料庫操作發生錯誤： {e}')
+            return
+        except Exception as e:
+            print(f'發生其它錯誤 {e}')
+            return
+
+    if not result:
+        print('找不到符合條件的電影')
+        return
+
+    list_rpt()
+    for row in result:
+        print_movie_row(row)
+    print()
+
+    new_title = input('請輸入新的電影名稱 (若不修改請直接按 Enter): ') or row['title']
+    new_director = input('請輸入新的導演 (若不修改請直接按 Enter): ') or row['director']
+    new_genre = input('請輸入新的類型 (若不修改請直接按 Enter): ') or row['genre']
+    new_year = input('請輸入新的上映年份 (若不修改請直接按 Enter): ') or row['year']
+    new_rating = input('請輸入新的評分 (1.0 - 10.0) (若不修改請直接按 Enter): ') or row['rating']
+
+    try: 
+        with connect_db() as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                UPDATE movies 
+                SET title = ?, director = ?, genre = ?, year = ?, rating = ? 
+                WHERE title LIKE ?
+            ''', (new_title, new_director, new_genre, new_year, new_rating, f'%{title}'))
+            conn.commit()
+            print('資料已修改')
+    except sqlite3.DatabaseError as e:
+        print(f'資料庫操作發生錯誤： {e}')
+    except Exception as e:
+        print(f'發生其它錯誤 {e}')
+
+def delete_movies():
+    y_n = input('刪除全部電影嗎？(y/n)： ')
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        try:
+            if y_n.lower() == 'y':
+                cursor.execute('DELETE FROM movies')
+                print(f'一共刪除了 {cursor.rowcount} 筆記錄')
+                return
+            else:
+            # elif y_n.lower() == 'n':
+                rm_title = input('請輸入要刪除的電影名稱：')
+                cursor.execute('SELECT * FROM movies WHERE title LIKE ?', (f'%{rm_title}%',))
+                result = cursor.fetchall()
+                list_rpt()
+                for row in result:
+                    print_movie_row(row)
+                yn = input('是否要刪除？(y/n)： ')
+                if yn.lower() == 'y':
+                    cursor.execute('DELETE FROM movies WHERE title LIKE ?', (f'%{rm_title}%',))
+                    print('電影已刪除')
+                else:
+                    return
+        except sqlite3.DatabaseError as e:
+            print(f'資料庫操作發生錯誤： {e}')
+        except Exception as e:
+            print(f'發生其它錯誤 {e}')
+
+def export_movies():
+    y_n = input('匯出全部電影嗎？(y_n)： ')
+    
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        try:
+            if y_n.lower() == 'y':
+                cursor.execute('SELECT * FROM movies')
+                result = cursor.fetchall()
+            else:
+                ex_title = input('請輸入要匯出的電影名稱： ')
+                cursor.execute('SELECT * FROM movies WHERE title LIKE ?', (f'%{ex_title}%',))
+                result = cursor.fetchall()
+
+            if not result:
+                print('找不到符合條件的電影')
+                return
+
+            movie_data = []
+            for row in result:
+                movie_data.append({
+                    'title': row['title'],
+                    'director': row['director'],
+                    'genre': row['genre'],
+                    'year': row['year'],
+                    'rating': row['rating']
+                })
+
+            with open(JSON_OUT_PATH, 'w', encoding='UTF-8') as f:
+                json.dump(movie_data, f, ensure_ascii=False, indent=4)
+            print('電影資料已匯出至 exported.json')
+        except sqlite3.DatabaseError as e:
+            print(f'資料庫操作發生錯誤： {e}')
+        except Exception as e:
+            print(f'發生其它錯誤 {e}')
+
+def connect_db():
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        conn.row_factory = sqlite3.Row  # 使用字典型別的 cursor
+        return conn
+    except sqlite3.OperationalError as e:
+        print(f'資料庫連接錯誤： {e}')
+        raise
+    except Exception as e:
+        print(f'發生其它錯誤 {e}')
+        raise
